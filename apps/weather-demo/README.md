2187d15aafaa48f55c8b7359e4c0aa565da145ed
Adrian Kuper <adrian.kuper@hotmail.de>
2018-10-15 19:50:48 +0200

    added demo app that fetches weather from OpenWeatherMap api

---
+++ apps/weather-demo/README.md
@@ -0,0 +1,21 @@
+# Snips Weather Demo
+
+Snips app that uses [OpenWeatherMap](https://openweathermap.org/) to fetch the current weather.
+
+## Intents
+
+* `kuper-adrian:showWeather`
+
+## How To Run
+
+1. Clone parent repo
+2. Navigate to this folder inside a terminal
+3. Execute `npm install`
+4. Get an API key from [OpenWeatherMap](https://openweathermap.org/)
+5. Create `secrets.json` file inside this directory and add the following contents (while replacing UPPERCASE values)
+```json
+{
+  "apiKey": "API_KEY_HERE"
+}
+```
+6. Execute `npm start`
---
+++ apps/weather-demo/routes/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const request = require('request');
+
+const config = require('../config.json')
+const secrets = require('../secrets.json')
+
+const router = express.Router();
+
+router.route('/')
+  .post((req, res) => {
+    const snipsMessage = req.body;
+    console.log(JSON.stringify(snipsMessage));
+
+    // poll OpenWeatherMap api for current weather ...
+    request({
+      url: `http://api.openweathermap.org/data/2.5/weather?q=${config.city},${config.country}&APPID=${secrets.apiKey}`,
+      method: 'GET'
+    }, (error, response, body) => {
+      let responseText = '';
+      const apiResult = JSON.parse(body);
+
+      // ... make response for assistant from result...
+      if (error) {
+        console.log(error.message);
+        responseText = `Error. ${error.message}`;
+      } else {
+        responseText = `The weather is ${apiResult.weather[0].main}. It is ${apiResult.main.temp - 273.15} degrees celsius.`
+      }
+
+      // ... and send it back
+      res.json({ responseText });
+    })
+  });
+
+module.exports = router;

20184582b8c3b75b13d811fce3c6c98db5c3e213
Adrian Kuper <adrian.kuper@hotmail.de>
2018-10-21 10:57:33 +0200

    removed useless code

--- snips-dashboard/routes/index.js
+++ snips-dashboard/routes/index.js
@@ -1,11 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-var test = require('socket.io')
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  test.toString
   res.render('index', { title: 'Snips Dashboard' });
 });
 

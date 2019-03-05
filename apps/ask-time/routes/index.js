8900dce276c68fc8086303ba9244bba2b4c5b5f0
Nilssteinmeyer <steinmeyer.nils@googlemail.com>
2019-03-05 16:45:01 +0100

    adde some more nicely and humanly sentence to Ask-Time

--- apps/ask-time/routes/index.js
+++ apps/ask-time/routes/index.js
@@ -14,14 +14,60 @@ router.route('/')
     const currentdate = new Date();
     const answerHours = currentdate.getHours();
     const answerMinutes = currentdate.getMinutes();
+    // name of owner for e.g. My Lord, Iron Man, Peter Griffin,... 
+    const name = `senior cyber developer `;
 
     if (answerHours < 10) {
       responseText = `Good Morning, it is ${answerHours} o'clock and  ${answerMinutes} Minutes. Have a nice Day`;
     } else if ( answerHours >= 10 && answerHours < 18) {
-      responseText = `It is ${answerHours} o'clock and  ${answerMinutes} Minutes.`;
+      responseText = GenerateGreedingSection() + ` It is ${answerHours} o'clock and  ${answerMinutes} Minutes.`;
     } else {
-      responseText = `Good Afternoon, it is ${answerHours} o'clock and  ${answerMinutes} Minutes. Have a nice evening`;
+      responseText = GenerateGreedingSection() + `, it is ${answerHours} o'clock and  ${answerMinutes} Minutes. Have a nice evening`;
     }
+
+    // not in use yet - prototype
+    function generateAnswer(answerHours,answerMinutes)
+    {
+      // do something
+      // generate greeting section of sentence
+      // generate middle of sentence
+      // generate ending of sentence
+      // maybe give a nice sentence of what world-day is today
+      return `dring some oetinger weizen!! `+ GenerateGreedingSection();
+    }
+
+    function GenerateGreedingSection()
+    {
+      var sentencOptions =
+      [
+        `Hello  Mr. ${name} `,
+        `Hey! `,
+        `ääääääöööhhh. What the fuck Mr. ${name}! I was sleeping so good. Okay, i will tell you something. `,
+        `Mr. ${name}. I am so blue I'm greener than purple. `,
+        `Mr. ${name}. I stepped on a Corn Flake, now I'm a Cereal Killer.`,
+        `Mr. ${name}. On a scale from one to ten what is your favourite colour of the alphabet.`,
+        `Mr. ${name}. Always remember: you're unique, just like everyone else.`,
+        `Mr. ${name}. When everything's coming your way, you're in the wrong lane.`,
+        `Mr. ${name}. I would never die for my beliefs because I might be wrong.`,
+        `Mr. ${name}. He who laughs last, didn't get it.`,
+        `Mr. ${name}. It is not my fault that I never learned to accept responsibility! `,
+        `Mr. ${name}. If you can’t live without me, why aren’t you dead yet? `,
+        `Good Morning Mr. ${name}.`,
+        `What is going on Mr. ${name}.`,
+        `Fuck, i need an Oetinger Weizen Beer  Mr. ${name}.`,
+        `Oh No Mr. ${name}.`,
+        `consequence also means to go wood ways to the end Mr. ${name}.`,
+        `Again? Mr. ${name}.`        
+      ]
+
+      var min = 1;
+      var max = sentencOptions.length;
+      var x = Math.round(Math.random() * (max - min)) + min;
+
+      return sentencOptions[x].toString();
+    }
+
+
     request({
       url: `http://localhost:3000/apps/askTime`,
       method: 'POST',
